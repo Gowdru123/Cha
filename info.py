41bf1782a70af9d4d80ad918d992c65953296671
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-08-22 20:55:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,7 +1,7 @@
 API_ID       = 29942004
 API_HASH     = "ad92f01e4a90cddebbea0ad16fa23026"
 BOT_TOKEN    = "5793915006:AAE4M4KDkjluFb58oylUK3Ir_0Y9vmuXw4I"
-SESSION      = ""
+SESSION      = "BQHI4PQAGfDhwyqzPvCQrfFhtp0r7ZH99m1eE6KAOI1YqRG2fC0Ah1XW-oRD_W658Nr-ClU9kRmsI8rB8MtGE3bgS5dw66jmLDNYy7G2l9IuF9onQzPcSGjIeo436ksjgU_FN1yf_p9Ht218z31CgdaIDiEUsc0TyzxLdIare-aMdff124m1araUae_J-UyHNGffySrMToewGQoDyYr0B81Ci3CbLDYg4AohaYizVkpoWakjkyQ8dpKCu0JFD1LDVv_nh-knioJoW8jbV_6al32ZqW_qaWrro9mOnA_Jg7Ulf8vy_4KNmQ53viUdZ18T-MrDgRNt72QtNl9CQx6D-o_vAS3TIAAAAABkI-HqAA"
 DATABASE_URI = "mongodb+srv://mangodb:mangodb@cluster0.dskkdky.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001835357010
 ADMIN        = 1680073194
