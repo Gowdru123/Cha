688f9e5613ad8e7c0fbf5dfc0ea3cf39ef7b4db3
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-08-22 20:53:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 29942004
+API_HASH     = "ad92f01e4a90cddebbea0ad16fa23026"
+BOT_TOKEN    = "5793915006:AAE4M4KDkjluFb58oylUK3Ir_0Y9vmuXw4I"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = 
-ADMIN        = 
-CHANNEL      = "@CyNiTeBaCkUp"
+DATABASE_URI = "mongodb+srv://mangodb:mangodb@cluster0.dskkdky.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001835357010
+ADMIN        = 1680073194
+
