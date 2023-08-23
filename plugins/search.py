38c62edfc5a5ed83cccdec5b660d20f81e490f0f
Gowdru123 <104635641+Gowdru123@users.noreply.github.com>
2023-08-23 23:43:23 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -25,7 +25,7 @@ async def search(bot, message):
                name = (msg.text or msg.caption).split("\n")[0]
                if name in results:
                   continue 
-               results += f"reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("{name}", callback_data=f"{msg.link}")]])\n\n"                                                      
+               results += f"<b><I> {name}\n {msg.link}</I></b>\n\n"
        if bool(results)==False:
           movies = await search_imdb(query)
           buttons = []
@@ -65,7 +65,7 @@ async def recheck(bot, update):
                name = (msg.text or msg.caption).split("\n")[0]
                if name in results:
                   continue 
-               results += f"reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("{name}", callback_data=f"{msg.link}")]])\n\n"
+               results += f"<b><I> {name}\n {msg.link}</I></b>\n\n"
        if bool(results)==False:          
           return await update.message.edit("Still no results found! Please Request here @rockersallmoviesearchbot", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🎯 Request To Admin 🎯", callback_data=f"request_{id}")]]))
        await update.message.edit(text=head+results, disable_web_page_preview=True)
